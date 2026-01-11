ce97f785b300fcd545ff1d02d881b87709db3080
padge81 <stuart@padgettservices.com.au>
2026-01-11 16:08:52 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
 
-# Create edid-rw directory in edid-emulator-webapp
-cd /edid-emulator-webapp
-sudo mkdir /edid-rw
-
-# Change into the edid-rw directory
-cd /edid-rw
-
-# Install prerequisites
-sudo apt-get install python3-smbus edid-decode
-
-# Clone the edid-rw repository
-sudo git clone https://github.com/bulletmark/edid-rw
-
-# Change into the edid-rw directory
-cd edid-rw
-
-# Read and display EDID information
-sudo./edid-rw 2 | edid-decode
+# Clone the edid-rw repository from GitHub
+git clone https://github.com/bulletmark/edid-rw
