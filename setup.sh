ad038917b5a50330c667eaecb41d1ceb0c2ce1cc
padge81 <stuart@padgettservices.com.au>
2026-01-11 16:15:02 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,11 @@
 
 # Clone the edid-rw repository from GitHub
 git clone https://github.com/bulletmark/edid-rw
+
+# Change to the edid-rw directory
+cd edid-rw
+
+# Run a test read
+echo "Running test read..."
+./edid-rw 2 | edid-decode
+echo "Test read completed."
